0581bf62dac3b5f7b932bc49e743532f55fc6311
i-am-anil <anil@v-comply.com>
2022-02-21 23:15:42 +0530

    statement updated

--- main.py
+++ main.py
@@ -42,10 +42,8 @@ while is_game_on:
 
     # Detect collission with tail
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             is_game_on = False
             score.game_over()
 
